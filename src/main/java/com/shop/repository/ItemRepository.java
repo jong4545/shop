f92a1636e9ad8c9c8a6305bc72d4c606290b47de
jong4545 <kg242242@gmail.com>
2023-09-17 14:22:10 +0900

    9월 17일 오류해결

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -20,7 +20,7 @@ public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredi
         @Query("select i from Item i where i.itemDetail like%:itemDetail% order by i.price desc")
         List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
-        @Query(value = "selct * from item i where i.item_detail like%:itemDetail% order by i.price desc", nativeQuery = true)
+        @Query(value = "select * from item i where i.item_detail like%:itemDetail% order by i.price desc", nativeQuery = true)
         List<Item> findByItemDetailByNative(@Param("itemDetail")String itemDetail);
 
 
